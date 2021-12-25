e16a9e12a01e03ed38e625f8a582956159d9bdcc
karthikeya4567 <95533986+karthikeya4567@users.noreply.github.com>
2021-12-25 19:40:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import joblib
-jk=joblib.load('IRIS FLOWER')
+jk=joblib.load('IRIS FLOWER')  
 st.title('SPECIES CLASSIFIER')
 ip=st.text_input('enter details')
 op=jk.predict([ip])
-if st.button ('Predict'):
-  st.button(op[0])
+if st.button ('Predict'):    
+  st.button(op[0])     
