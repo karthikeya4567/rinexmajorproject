62f5db498f301bd82c203aaf8aefc2dc6253f5b0
karthikeya4567 <95533986+karthikeya4567@users.noreply.github.com>
2021-12-26 00:49:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ sw = st.slider('Sepal Width', 2.0, 4.4, 0.5)
 pl = st.slider('Petal Length', 1.0, 6.9, 0.5)
 pw = st.slider('Petal Width', 0.1,2.5, 0.5)
 
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_iris    
 iris = load_iris()
 
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier   
 model = DecisionTreeClassifier()
-model.fit(iris.data,iris.target)
+model.fit(iris.data,iris.target)   
 
 op = model.predict([[sl,sw,pl,pw]])
 op = iris.target_names[op[0]]
