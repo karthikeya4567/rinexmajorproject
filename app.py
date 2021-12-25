88dcdbef36382f08c1f4fe380994439bcc9b9cdf
karthikeya4567 <95533986+karthikeya4567@users.noreply.github.com>
2021-12-26 00:40:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
-st.title("Iris Classifier- API")
+st.title('SPECIES CLASSIFIER')
 
 sl = st.slider('Sepal Length', 4.3, 7.9, 0.5)
-sw = st.slider('Sepal Width', 2.0, 4.4, 0.5)     
+sw = st.slider('Sepal Width', 2.0, 4.4, 0.5)  
 pl = st.slider('Petal Length', 1.0, 6.9, 0.5)
 pw = st.slider('Petal Width', 0.1,2.5, 0.5)
 
@@ -16,3 +16,5 @@ model.fit(iris.data,iris.target)
 op = model.predict([[sl,sw,pl,pw]])
 op = iris.target_names[op[0]]
 st.title(op)
+
+
